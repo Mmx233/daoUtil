13d8f61443c38aa60b687ccec36bb869a08b4f46
Mmx <1624045573@qq.com>
2021-08-15 16:27:41 +0800

    feat:实现功能

---
+++ dao.go
@@ -0,0 +1,74 @@
+package daoUtil
+
+import "gorm.io/gorm"
+
+var db *gorm.DB
+
+func Init(DB *gorm.DB) {
+	db = DB
+}
+
+func Begin() *gorm.DB {
+	return db.Begin()
+}
+
+func DefaultInsert(a interface{}) error {
+	return DefaultInsertTx(db, a)
+}
+
+func DefaultInsertTx(tx *gorm.DB, a interface{}) error {
+	return tx.Create(a).Error
+}
+
+func DefaultDelete(a interface{}) error {
+	return DefaultDeleteTx(db, a)
+}
+
+func DefaultDeleteTx(tx *gorm.DB, a interface{}) error {
+	return tx.Where(a).Delete(a).Error
+}
+
+func DefaultFind(a interface{}) error {
+	return DefaultFindTx(db, a)
+}
+
+func DefaultFindTx(tx *gorm.DB, a interface{}) error {
+	return tx.Where(a).Find(a).Error
+}
+
+func DefaultExist(a interface{}) bool {
+	return DefaultExistTx(db, a)
+}
+
+func DefaultExistTx(tx *gorm.DB, a interface{}) bool {
+	var t struct {
+		ID uint
+	}
+	tx.Model(a).Where(a).Find(&t)
+	return t.ID != 0
+}
+
+func DefaultGet(a interface{}) error {
+	return DefaultGetTx(db, a)
+}
+
+func DefaultGetTx(tx *gorm.DB, a interface{}) error {
+	return tx.Find(a).Error
+}
+
+func DefaultGetWhitQuery(a interface{}, t interface{}) error {
+	return DefaultGetWhitQueryTx(db, a, t)
+}
+
+func DefaultGetWhitQueryTx(tx *gorm.DB, a interface{}, t interface{}) error {
+	return tx.Where(a).Find(t).Error
+}
+
+func DefaultCounter(t interface{}) (int64, error) {
+	return DefaultCounterTx(db, t)
+}
+
+func DefaultCounterTx(tx *gorm.DB, t interface{}) (int64, error) {
+	var n int64
+	return n, tx.Where(t).Count(&n).Error
+}
