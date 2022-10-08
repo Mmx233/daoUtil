7bb030ede4d32f423ef1fa337608d75b94b3e994
Mmx233 <admin@mmxblog.com>
2022-10-09 00:59:27 +0800

    feat: 移除 servicePackage

--- dao.go
+++ dao.go
@@ -1,61 +1,35 @@
 package daoUtil
 
 import (
-	"context"
 	"gorm.io/gorm"
 )
 
-type DaoUtil struct {
-	DB *gorm.DB
-}
-
-func (s *DaoUtil) Begin() (*gorm.DB, error) {
-	tx := s.DB.Begin()
-	return tx, tx.Error
-}
-
-func (s *DaoUtil) BeginService(p Service) (Service, error) {
-	tx := s.DB.Begin().WithContext(context.WithValue(context.Background(), packageKey, &Context{}))
-	p.fill(tx)
-	return p, tx.Error
-}
-
-func (s *DaoUtil) NewZeroService(p Service) Service {
-	p.fill(s.DB)
-	return p
-}
-
-func (s *DaoUtil) DefaultInsert(tx *gorm.DB, a interface{}) error {
+func Insert(tx *gorm.DB, a interface{}) error {
 	return tx.Create(a).Error
 }
 
-func (s *DaoUtil) DefaultDelete(tx *gorm.DB, a interface{}) error {
+func Delete(tx *gorm.DB, a interface{}) error {
 	return tx.Where(a).Delete(a).Error
 }
 
-func (s *DaoUtil) DefaultFind(tx *gorm.DB, a interface{}) error {
+func Find(tx *gorm.DB, a interface{}) error {
 	return tx.Where(a).Find(a).Error
 }
 
-func (s *DaoUtil) DefaultExist(tx *gorm.DB, a interface{}) (bool, error) {
+func Exist(tx *gorm.DB, a interface{}) (bool, error) {
 	var t bool
 	return t, tx.Model(a).Select("1").Where(a).Find(&t).Error
 }
 
-func (s *DaoUtil) DefaultGet(tx *gorm.DB, a interface{}) error {
+func Get(tx *gorm.DB, a interface{}) error {
 	return tx.Find(a).Error
 }
 
-func (s *DaoUtil) DefaultGetWhitQuery(tx *gorm.DB, a interface{}, t interface{}) error {
+func GetWhitQuery(tx *gorm.DB, a interface{}, t interface{}) error {
 	return tx.Where(a).Find(t).Error
 }
 
-func (s *DaoUtil) DefaultCounter(tx *gorm.DB, t interface{}) (int64, error) {
+func Counter(tx *gorm.DB, t interface{}) (int64, error) {
 	var n int64
 	return n, tx.Model(t).Where(t).Count(&n).Error
-}
-
-func (s *DaoUtil) DefaultLock(tx *gorm.DB, t interface{}) (bool, error) {
-	var r bool
-	return r, LockForUpdate(tx).Select("1").Model(t).Where(t).Find(&r).Error
 }
--- transfer.go
+++ transfer.go
@@ -2,24 +2,4 @@ package daoUtil
 
 import "gorm.io/gorm"
 
-const packageKey = "DaoServicePackage"
-
-type Context struct {
-	Ended bool
-	ES    []func() error
-	EF    []func()
-}
-
-type Model interface {
-	Lock(tx *gorm.DB) (bool, error)
-}
-
-type Service interface {
-	db() *gorm.DB
-	fill(tx *gorm.DB)
-	LockOrRoll(m Model) (bool, error)
-	RollBack() error
-	Commit() error
-}
-
 type ServiceOpt func(tx *gorm.DB) *gorm.DB
